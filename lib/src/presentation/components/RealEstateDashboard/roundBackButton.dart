d53a7d00390b2f0f14ebd21ae5e3d0a6aa84dbad
Muhib370621 <mubibarif@gmail.com>
2022-11-28 10:00:29 +0500

    add screen merged unsuccessfully

---
+++ lib/src/presentation/components/RealEstateDashboard/roundBackButton.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:twiddle_refactored/src/core/utils/app_colors.dart';
+
+class RoundBackButton extends StatelessWidget {
+  Color color;
+  double size;
+  Color iconColor;
+  RoundBackButton({
+    this.color = AppColors.mainColor,
+    this.size = 14,
+    this.iconColor = AppColors.mainColor,
+    Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: (){
+          Get.back();
+        },
+        child: Container(
+          padding: EdgeInsets.all(10.sp),
+          decoration: BoxDecoration(
+            color: color,
+            shape: BoxShape.circle,
+          ),
+          child: Icon(
+            Icons.arrow_back_ios_rounded,
+            color: iconColor,
+            size: size.sp,
+          ),
+        )
+    );
+  }
+}
